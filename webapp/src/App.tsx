a9baf957f59032f1190abef50dd69ca8977586e6
Jason Rundell <jason.rundell@gmail.com>
2025-11-20 01:45:24 -0500

    fix: Correct import path for Index page in App component

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import Index from "./pages/index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
