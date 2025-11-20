adacfdf6deb315233c07d1370b9f1fbc1830a42d
Jason Rundell <jason.rundell@gmail.com>
2025-11-20 01:40:07 -0500

    chore: Update Vercel configuration by removing unused buildCommand and framework fields; add React plugin to Vite configuration for improved development experience.

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  plugins: [react()],
   server: {
     host: "::",
     port: 3000,
